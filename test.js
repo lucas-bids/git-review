c0f8f1f064ec36d0d78db7a165864095a25c21c2
Lucas Vidal <lucasvidal@MacBook-Air-de-Lucas.local>
2023-07-28 13:25:10 -0300

    feat(app): test code block color on push

--- test.js
+++ test.js
@@ -14,3 +14,27 @@ b = temp;
 
 console.log(`The value of a after swapping: ${a}`);
 console.log(`The value of b after swapping: ${b}`);
+
+// program to check an Armstrong number of three digits
+
+let sum = 0;
+const number = prompt('Enter a three-digit positive integer: ');
+
+// create a temporary variable
+let temp2 = number;
+while (temp2 > 0) {
+    // finding the one's digit
+    let remainder = temp2 % 10;
+
+    sum += remainder * remainder * remainder;
+
+    // removing last digit from the number
+    temp2 = parseInt(temp2 / 10); // convert float into integer
+}
+// check the condition
+if (sum == number) {
+    console.log(`${number} is an Armstrong number`);
+}
+else {
+    console.log(`${number} is not an Armstrong number.`);
+}
